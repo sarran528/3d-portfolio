56acdb3b0de8c89c8dd58bf4ac7a610b0fa13976
sarran528 <sarranm528@gmail.com>
2025-07-01 23:07:11 +0530

    optomized the road

--- src/App.tsx
+++ src/App.tsx
@@ -185,7 +185,7 @@ function App() {
             text="Drive"
             onClick={handleDriveMode}
           />
-{/* dfghjkl;fghjkl;xcvbj */}
+
           <Walls />
           <CityArch />
           <CityNameBoard name="SARRAN" position={[-15, 0, 10]} />
