89d2d2b7491546d8223b9fbdfe2b409602efccf3
sarran528 <sarranm528@gmail.com>
2025-07-03 23:53:35 +0530

    camera duplication managded

--- src/App.tsx
+++ src/App.tsx
@@ -185,7 +185,7 @@ function App() {
             text="Drive"
             onClick={handleDriveMode}
           />
-{/* sjdddddsdvkmsdnvodnfvndfsnodddd */}
+{/* sjdddddsdvkmsdnvodnfvndfsnolkjhgfdcxcvghjkl';lkjbvdddd */}
           <Walls />
           <CityArch />
           <CityNameBoard name="SARRAN" position={[-15, 0, 10]} />
