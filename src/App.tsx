395d9e3c1026449a1deca626ef267d6267b82a27
sarran528 <sarranm528@gmail.com>
2025-07-03 23:54:41 +0530

    statue position altered

--- src/App.tsx
+++ src/App.tsx
@@ -185,7 +185,7 @@ function App() {
             text="Drive"
             onClick={handleDriveMode}
           />
-
+//sijdfedfewdfe
           <Walls />
           <CityArch />
           <CityNameBoard name="SARRAN" position={[-15, 0, 10]} />
